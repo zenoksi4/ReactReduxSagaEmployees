4aea2f408704f1e908b39df46b55236b8fcd4eef
Pasha <zenoken4ik@gmail.com>
2023-01-30 18:05:06 +0200

    added requests with Redux states

--- src/components/Jobs/index.jsx
+++ src/components/Jobs/index.jsx
@@ -1,10 +1,27 @@
+import { apiActions } from "modules/api/actions";
 import api from "modules/api/api";
 import { JOBS } from "modules/api/endpoints";
 import { useEffect } from "react"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Jobs() {
-    const state = useSelector(state => state)
+    const state = useSelector(state => state.api[JOBS]);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        async function fetchData() {
+            try {  
+                dispatch(apiActions.fetch(JOBS))
+
+                const data = await api.fetch(JOBS);
+
+                dispatch(apiActions.fetchSuccess(JOBS, data))
+            } catch(e) {
+                dispatch(apiActions.fetchFailure(JOBS, e))
+
+            }
+        } 
+        fetchData();}, [])
     console.log(state)
 
     return(
--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware, combineReducers, compose} from 'redux';
 import apiReducer from 'modules/api/reducer';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ 
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
 const reducers = combineReducers({
     api: apiReducer
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, composeEnhancers());
 
 export default store;
